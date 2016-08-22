e7046748251a56c67f9b4a428408b4b2b265c1b2
Danilo Chaves <chaves_danilo@emerson.edu>
2016-08-21 21:49:39 -0400

    final version

--- FunFacts/FactModel.swift
+++ FunFacts/FactModel.swift
@@ -12,7 +12,7 @@ struct FactModel{
         "Ants stretch when they wake up in the morning.",
         "Ostriches can run faster than horses.",
         "Olympic gold medals are actually made mostly of silver.",
-        "You are born with 300 bones; by the time you are an adult you will have 206.",
+        "You are born with 300 bones; by the time you are an adult you will have 206.", //This is a test comment for gitHub
         "It takes about 8 minutes for light from the Sun to reach Earth.",
         "Some bamboo plants can grow almost a meter in just one day.",
         "The state of Florida is bigger than England.",
